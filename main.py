0756b0201ba61deaac6a2595f2396ef54518f5c4
TheAnimeEnthusiast <mritvik02@gmail.com>
2026-03-13 20:56:22 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#Write a program to calculate the sum of whole numbers.
+
+n = int(input("From what number do you want me to add?"))
+
+s = 0
+
+for i in range(1,n):
+    s = i+s
+    print(s)
+
+
+
+
+
